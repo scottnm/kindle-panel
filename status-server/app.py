c5eee06c82936119ccd2e76db030c2d73674ace4
Scott Munro <scottnmunro@gmail.com>
2022-12-29 15:49:45 -0800

    downloading a screenshot from the flask server

--- status-server/app.py
+++ status-server/app.py
@@ -1,5 +1,6 @@
 import flask
 import json
+import html2image
 
 app = flask.Flask(__name__)
 
@@ -10,6 +11,17 @@ def index():
     with open(PANEL_DATA_JSON_FILE, 'r') as f:
         panel_data = json.loads(f.read())
         return flask.render_template('index.html', status=panel_data["status"])
+
+@app.route('/get_status_screenshot')
+def get_status_screenshot():
+    hti = html2image.Html2Image()
+    status=None
+    panel_data = None
+    with open(PANEL_DATA_JSON_FILE, 'r') as f:
+        panel_data = json.loads(f.read())
+    html_str = flask.render_template('index.html', status=panel_data["status"])
+    hti.screenshot(html_str=html_str, save_as='kindle_panel_screenshot.png', size=(600, 800))
+    return flask.send_file('kindle_panel_screenshot.png', mimetype='image/png')
 
 @app.route('/set_status')
 def set_status():
