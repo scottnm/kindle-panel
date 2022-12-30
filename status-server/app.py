bf3cc9e4ae10b06fb8ae88cf4ad3e767e90fecc6
Scott Munro <scottnmunro@gmail.com>
2022-12-30 14:11:39 -0800

    refacotr to create file if not present

--- status-server/app.py
+++ status-server/app.py
@@ -1,39 +1,51 @@
 import flask
 import json
 import html2image
+import tempfile
+import os
 
 app = flask.Flask(__name__)
+
+# set the file max age to 5 seconds to work around browser caching file responses
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 5
 
-PANEL_DATA_JSON_FILE = "/tmp/kindle_panel_data.json"
+PANEL_DATA_JSON_FILE = os.path.join(tempfile.gettempdir(), "kindle_panel_data.json")
+
+def load_panel_data():
+    try:
+        with open(PANEL_DATA_JSON_FILE, 'r') as f:
+            return json.loads(f.read())
+    except FileNotFoundError:
+        print("Panel data not found @ %s! Creating and returning default data" % PANEL_DATA_JSON_FILE)
+        default_panel_data = { "status": "unset" }
+        store_panel_data(default_panel_data)
+        return default_panel_data
+
+def store_panel_data(panel_data):
+    with open(PANEL_DATA_JSON_FILE, 'w') as f:
+        data_str = json.dumps(panel_data, indent=4)
+        f.write(data_str)
 
 @app.route('/')
 def index():
-    with open(PANEL_DATA_JSON_FILE, 'r') as f:
-        panel_data = json.loads(f.read())
-        return flask.render_template('index.html', status=panel_data["status"])
+    panel_data = load_panel_data()
+    return flask.render_template('index.html', status=panel_data["status"])
 
 @app.route('/get_status_screenshot')
 def get_status_screenshot():
     hti = html2image.Html2Image()
-    status=None
-    panel_data = None
-    with open(PANEL_DATA_JSON_FILE, 'r') as f:
-        panel_data = json.loads(f.read())
+    panel_data = load_panel_data()
     status = panel_data["status"]
     html_str = flask.render_template('index.html', status=status)
-    print("Returning screnshot with status ", status)
     hti.screenshot(html_str=html_str, save_as='kindle_panel_screenshot.png', size=(600, 800))
     return flask.send_file('kindle_panel_screenshot.png', mimetype='image/png')
 
 @app.route('/set_status')
 def set_status():
     query_params = flask.request.args.to_dict()
-    print(query_params["status"])
-    updated_panel_data_json = json.dumps({ "status": query_params["status"] }, indent=4)
-    with open(PANEL_DATA_JSON_FILE, 'w') as f:
-        f.write(updated_panel_data_json)
-    return flask.render_template('set_status.html', status=query_params["status"])
+    status = query_params["status"]
+    store_panel_data({ "status":  status })
+    return flask.render_template('set_status.html', status=status)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
