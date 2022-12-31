3d7445459e25b4a64426a8bc05f441ffeed70c0b
Scott Munro <scottnmunro@gmail.com>
2022-12-31 00:31:28 -0800

    checking refresh script

---
+++ refresh_panel.sh
@@ -0,0 +1,4 @@
+curl lemonpie:5000/get_status_screenshot -o status.png
+eips -c
+eips -c
+eips -g status.png
--- status-server/capture.py
+++
@@ -1,3 +0,0 @@
-from html2image import Html2Image
-hti = Html2Image()
-hti.screenshot(url='https://www.python.org', save_as='python_org.png', size=(600, 800))
